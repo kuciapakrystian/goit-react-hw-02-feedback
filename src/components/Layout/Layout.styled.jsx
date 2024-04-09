abb0903739cb3be859b1473d69accb84c0799302
kuciapakrystian <kuciapakrystian@outlook.com>
2024-04-09 20:02:15 +0200

    Delete Layout.styled.jsx

--- src/components/Layout/Layout.styled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: ${p => p.theme.space[4]}px;
-  padding: ${p => p.theme.space[4]}px;
-`;
